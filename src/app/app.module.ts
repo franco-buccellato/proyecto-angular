127eceb3538aaf00ea8a93cd92d76d9c61abf468
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2022-09-22 22:32:22 -0300

    Primer Entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { StudentComponent } from './componentes/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
